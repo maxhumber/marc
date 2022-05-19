c3bc683d801288f685eb9a3414cf1eeaadca6029
Max Humber <max.humber@gmail.com>
2022-05-19 13:08:30 +0100

    better ergonomics

--- Marc/Sources/Marc/MyPlayground.playground/Contents.swift
+++ Marc/Sources/Marc/MyPlayground.playground/Contents.swift
@@ -1,54 +1,71 @@
 import Foundation
 
-struct MarkovPair {
-    var from: String
-    var to: String
-}
-
-class MarkovChain {
-    var store: [String: [String: Int]] = [:]
+class MarkovChain<Element: Hashable> {
+    var store: [Element: [Element: UInt]] = [:]
     
-    func add(from: String, to: String) {
-        store[from, default: [:]][to, default: 0] += 1
+    init() {}
+    
+    init(_ sequence: [Element]) {
+        for (a, b) in zip(sequence, sequence[1...]) {
+            update(a, b)
+        }
+    }
+    
+    subscript(a: Element) -> [Element: UInt] {
+        store[a, default: [:]]
+    }
+    
+    func update(_ a: Element, _ b: Element) {
+        store[a, default: [:]][b, default: 0] += 1
+    }
+    
+    func after(_ a: Element) -> Element? {
+        let options = store[a, default: [:]]
+        let total = options.values.reduce(0, +)
+        let rand = UInt(arc4random_uniform(UInt32(total)))
+        var sum = UInt(0)
+        for (option, weight) in options {
+            sum += weight
+            if rand < sum {
+                return option
+            }
+        }
+        return nil
     }
 }
 
-let chain = MarkovChain()
-chain.add(from: "R", to: "P")
+let chain = MarkovChain<String>()
+chain.update("R", "P")
+chain.update("R", "P")
+chain.update("R", "P")
+chain.update("R", "R")
+chain.update("R", "S")
+chain.update("R", "S")
+chain.update("R", "S")
+chain.update("S", "S")
+chain.after("R")
+chain.after("R")
+chain.after("R")
+chain.after("S")
+chain["R"]
+
+let sequence = [
+    "Rock", "Rock", "Rock", "Paper", "Rock", "Scissors",
+    "Paper", "Paper", "Scissors", "Rock", "Scissors",
+    "Scissors", "Paper", "Scissors", "Rock", "Rock", "Rock",
+    "Paper", "Scissors", "Scissors", "Scissors", "Rock"
+]
+
+let chain2 = MarkovChain(sequence)
+chain2.after("Rock")
+chain2.after("Rock")
+chain2.after("Rock")
+chain2.after("Rock")
+chain2.after("Rock")
+chain2.after("Rock")
+
+
+
 print(chain.store)
 
 
-var store: [String: [String: Int]] = [:]
-
-let pair = MarkovPair(from: "R", to: "P")
-
-store["R"] = [:]
-store["R"]?["P"] = 1
-if let value = store["R"]?["P"] {
-    store["R"]?["P"] = value + 1
-}
-
-store["R", default: [:]]["P", default: 0] += 1
-store["R", default: [:]]["R", default: 0] += 1
-store
-
-
-let input = "R"
-let output = "S"
-
-store["S", default: [:]]
-
-
-
-
-if let options = store[input] {
-    
-} else {
-    store[input] = [:]
-}
-
-
-
-print(store)
-
-store["R"]
