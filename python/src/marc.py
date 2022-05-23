5e7f2a77df9a5bdd1c9b271f55600fd79058862d
Max Humber <max.humber@gmail.com>
2022-05-23 10:21:09 +0100

    docstrings

--- python/src/marc.py
+++ python/src/marc.py
@@ -4,25 +4,50 @@ import random
 
 class MarkovChain:
     def __init__(self, sequence=None):
-        """Doc string"""
+        """Instantiate chain with/without starting sequence
+
+        Examples:
+        ```
+        chain = MarkovChain(["R", "P", "S"])
+        chain2 = MarkovChain()
+        ```
+        """
         self._store = defaultdict(lambda: defaultdict(lambda: 0))
         if not sequence:
             return
         for a, b in zip(sequence, sequence[1:]):
             self.update(a, b)
 
-    def __getitem__(self, element):
-        """Doc string"""
-        options = sorted(self._store[element].items(), key=lambda i: -i[1])
-        total = sum(self._store[element].values())
+    def __getitem__(self, state):
+        """Fetch transition probabilities for state
+
+        Example:
+        ```
+        probs = chain["R"]
+        ```
+        """
+        options = sorted(self._store[state].items(), key=lambda i: -i[1])
+        total = sum(self._store[state].values())
         return {option: weight / total for (option, weight) in options}
 
     def update(self, a, b):
-        """Doc string"""
+        """Update chain with transition a -> b
+
+        Example:
+        ```
+        chain.update("R", "P")
+        ```
+        """
         self._store[a][b] += 1
 
     def next(self, after):
-        """Doc string"""
+        """Generate next state from chain
+
+        Example:
+        ```
+        chain.next("R")
+        ```
+        """
         options = list(self._store[after].keys())
         weights = self._store[after].values()
         return random.choices(options, weights, k=1)[0]
